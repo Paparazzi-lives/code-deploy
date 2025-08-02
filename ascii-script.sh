de805d7ac89abd0f87aeadebaa482f496aaa7b39
APPAU BISMARK <appaubis@yahoo.com>
2025-08-02 22:03:09 +0000

    Update ascii-script.sh

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for for your life, I am a DRAGON......RAWRRRRRR" >> dragon.txt
+cowsay -f dragon "Run for for your life, I am a the Dangerous DRAGON......RAWRRRRRR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt 
 ls -ltra
